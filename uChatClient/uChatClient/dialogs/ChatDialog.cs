38a3e592ae257bfb788e11af83443b44ab9b08cc
Overfly3 <a-theijs-moc@hotmail.de>
2015-03-29 18:21:06 +0200

    update
    udpate

---
+++ uChatClient/uChatClient/dialogs/ChatDialog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uChatClient.dialogs
+{
+    public partial class ChatDialog : Form
+    {
+        public ChatDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
