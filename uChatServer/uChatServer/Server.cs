8bf050b249584fd2c23f71bb526726986ee463f0
lulux3 <luca.tirabassi@hotmail.com>
2015-07-05 18:38:46 +0200

    Packet
    dded packet methods

--- uChatServer/uChatServer/Server.cs
+++ uChatServer/uChatServer/Server.cs
@@ -4,53 +4,136 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Xml.Serialization;
 using uChatServer.Entities;
 
-namespace Server
+namespace uChatServer
 {
-    class Program
+    internal class Program
     {
-        const int PORT_NO = 5000;
-        const string SERVER_IP = "127.0.0.1";
+        private const int PortNo = 5000;
+        private const string ServerIp = "127.0.0.1";
 
-        static void Main(string[] args)
+        private Dictionary<string, TcpClient> _users;
+
+        private static void Main(string[] args)
         {
+            var program = new Program { _users = new Dictionary<string, TcpClient>() };
 
             while (true)
             {
-                startListening();
+                program.StartListening();
             }
         }
 
-        public static void startListening()
+        private void StartListening()
         {
-            IPAddress localAdd = IPAddress.Parse(SERVER_IP);
-            TcpListener listener = new TcpListener(localAdd, PORT_NO);
+            var localAdd = IPAddress.Parse(ServerIp);
+            var listener = new TcpListener(localAdd, PortNo);
+
             Console.WriteLine("Listening...");
             listener.Start();
-            TcpClient client = listener.AcceptTcpClient();
-            NetworkStream nwStream = client.GetStream();
-            StreamReader sr = new StreamReader(nwStream);
 
-            Packet newPacket;
-            string receivedString = sr.ReadToEnd();
+            var client = listener.AcceptTcpClient();
+            var nwStream = client.GetStream();
+            var sr = new StreamReader(nwStream);
 
-            newPacket = DeserializeToObject(receivedString);
+            var receivedString = sr.ReadToEnd();
+
+            var newPacket = DeserializeToObject(receivedString);
+
+            HandlePacket(newPacket, client);
 
             client.Close();
             listener.Stop();
         }
-        public static string SerializeToString(Packet packet)
+
+        private void HandlePacket(Packet newPacket, TcpClient client)
         {
-            string serializedData = string.Empty;
+            switch (newPacket.PacketType)
+            {
+                case PacketType.Message:
+                    Send(newPacket, newPacket.Message, client);
+                    break;
 
-            XmlSerializer serializer = new XmlSerializer(packet.GetType());
+                case PacketType.GetOnlineUsers:
+                    SendOnlineUsers(newPacket, client);
+                    break;
 
-            using (StringWriter sw = new StringWriter())
+                case PacketType.Login:
+                    HandleLogin(newPacket, client);
+                    break;
+
+                case PacketType.Logout:
+                    HandleLogout(newPacket, client);
+                    break;
+            }
+        }
+
+        private void HandleLogout(Packet newPacket, TcpClient client)
+        {
+            if (_users.ContainsKey(newPacket.SenderIP))
+            {
+                _users.Remove(newPacket.SenderIP);
+            }
+            Send(newPacket, "true", client);
+        }
+
+        private void SendOnlineUsers(Packet newPacket, TcpClient client)
+        {
+
+            string onlineUsersString = _users.Aggregate<KeyValuePair<string, TcpClient>, string>(null, (current, user) => current + (user.Key + ";" + ((IPEndPoint)user.Value.Client.RemoteEndPoint).ToString() + "/"));
+            Send(newPacket, onlineUsersString, client);
+        }
+
+        private void HandleLogin(Packet newPacket, TcpClient client)
+        {
+            string success = "false";
+
+            if (!_users.ContainsKey(newPacket.SenderIP))
+            {
+                _users.Add(newPacket.SenderIP, client);
+                success = "true";
+            }
+
+            Send(newPacket, success, client);
+        }
+
+        private void Send(Packet newPacket, string str, TcpClient client)
+        {
+            try
+            {
+                var nwStream = client.GetStream();
+                var sr = new StreamWriter(nwStream);
+
+                var packet = new Packet
+                {
+                    Message = str,
+                    PacketType = newPacket.PacketType,
+                    SenderIP = newPacket.SenderIP,
+                    ReceiverIP = newPacket.ReceiverIP
+                };
+
+                var serializedPacket = SerializeToString(packet);
+
+                sr.WriteLine(serializedPacket);
+
+                sr.Flush();
+                sr.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private string SerializeToString(Packet packet)
+        {
+            string serializedData;
+
+            var serializer = new XmlSerializer(packet.GetType());
+
+            using (var sw = new StringWriter())
             {
                 serializer.Serialize(sw, packet);
                 serializedData = sw.ToString();
@@ -58,11 +141,11 @@ namespace Server
             return serializedData;
         }
 
-        public static Packet DeserializeToObject(string data)
+        private Packet DeserializeToObject(string data)
         {
             Packet deserializedPacket;
 
-            XmlSerializer deserializer = new XmlSerializer(typeof(Packet));
+            var deserializer = new XmlSerializer(typeof(Packet));
             using (TextReader tr = new StringReader(data))
             {
                 deserializedPacket = (Packet)deserializer.Deserialize(tr);
