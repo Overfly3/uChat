9858cf5eb216e38676a4606072465f724e39b312
lulux3 <luca.tirabassi@hotmail.com>
2015-07-05 16:56:20 +0200

    Packet
    Added packets

--- uChat Client/uChat Client/Entities/Packet.cs
+++ uChat Client/uChat Client/Entities/Packet.cs
@@ -1,23 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace uChat_Client.Entities
 {
     [Serializable]
     public enum PacketType
     {
-        Login
+        Login,
+        GetOnlineUsers,
+        Logout,
+        Message,
+        GetOnlineState
     }
+
     [Serializable]
     public class Packet
     {
         public PacketType PacketType { get; set; }
+
         public string Message { get; set; }
+
         public string SenderIP { get; set; }
+
         public string ReceiverIP { get; set; }
     }
 }
--- uChatServer/uChatServer/Server.cs
+++ uChatServer/uChatServer/Server.cs
@@ -19,6 +19,7 @@ namespace Server
 
         static void Main(string[] args)
         {
+
             while (true)
             {
                 startListening();
@@ -27,31 +28,13 @@ namespace Server
 
         public static void startListening()
         {
-            //---listen at the specified IP and port no.---
             IPAddress localAdd = IPAddress.Parse(SERVER_IP);
             TcpListener listener = new TcpListener(localAdd, PORT_NO);
             Console.WriteLine("Listening...");
             listener.Start();
-
-            //---incoming client connected---
             TcpClient client = listener.AcceptTcpClient();
-
-            //---get the incoming data through a network stream---
             NetworkStream nwStream = client.GetStream();
             StreamReader sr = new StreamReader(nwStream);
-
-            //byte[] buffer = new byte[client.ReceiveBufferSize];
-
-            ////---read incoming stream---
-            //int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
-
-            ////---convert the data received into a string---
-            //string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-            //Console.WriteLine("Received : " + dataReceived);
-
-            ////---write back the text to the client---
-            //Console.WriteLine("Sending back : " + dataReceived);
-            //nwStream.Write(buffer, 0, bytesRead);
 
             Packet newPacket;
             string receivedString = sr.ReadToEnd();
